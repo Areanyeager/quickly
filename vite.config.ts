b58b15ba8ebb649d2f9dbc8325b03294c057c0c5
Areanyeager <af7871460@gmail.com>
2025-06-24 01:42:19 +0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // 👇 Replace 'your-repo-name' with your actual GitHub repo name
 export default defineConfig({
   base: '/quickly/',
+  server: {
+    port: 8080
+  },
   plugins: [react()]
 });
